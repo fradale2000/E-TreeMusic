f757774560e44d31b49d7fa829f6f6d273e9c2f3
fradale2000 <fradale2000@gmail.com>
2022-06-28 16:30:41 +0200

    commit sviluppo

--- prodotti.js
+++ prodotti.js
@@ -1,4 +1,5 @@
 // by fradale2000
+//vari contatori per for oppure crezione di oggetti
 var cont = localStorage.length;
 var cont_prod = 0;
 var cont_canzone = 0;
@@ -6,6 +7,7 @@ var cont_artista = 0;
 var cont_tot = 0;
 var lista_prodotti= [];
 var div = document.getElementById("div_prodotti");
+//creo un tot di oggetti tramite for
 for (let i = 0; i < 10; i++) {
     let prod= {
         "IDProd":cont_prod,
@@ -17,7 +19,7 @@ for (let i = 0; i < 10; i++) {
         "Costo":3.50,
         "scr": "Immagini/TestLogo.png",             
     }
-
+    //salvo tutto nel localstorage
     localStorage.setItem("account_" +cont_tot,JSON.stringify(prod));
     // localStorage.setItem("carello" +cont_tot,JSON.stringify(carello));
     lista_prodotti.push(prod);
@@ -28,21 +30,25 @@ for (let i = 0; i < 10; i++) {
 }
 // localStorage.setItem("lista_prodotti" +cont_tot,JSON.stringify(lista_prodotti));
 console.log(lista_prodotti);
-
+//ciclo per creare tutti gli oggetti nella pagina in maniera dinamica
 for (let i = 0; i < lista_prodotti.length; i++) {
     var prod = lista_prodotti[i];   
     console.log(prod);
+    //creo un div per inserire la canzone in maniera dinamica
     let prodotto = document.createElement("div");
+    //aggiungo la classe song
     prodotto.classList.add("song");
     prodotto.setAttribute("id",prod.IDProd);
+    //aggiungo un'id
+    let immagine = document.createElement();
     let titolo = document.createElement("span");
-    titolo.innerHTML= `<span>Titolo : ${prod.Titolo}</span>`;
+    titolo.innerHTML= `<span>${prod.Titolo}</span>`;
     let artista = document.createElement("span");
-    artista.innerHTML= `<span>Artista : ${prod.Artista}</span>`;
+    artista.innerHTML= `<span>${prod.Artista}</span>`;
     let anno = document.createElement("span");
-    anno.innerHTML= `<span>Anno : ${prod.Anno}</span>`;
+    anno.innerHTML= `<span>${prod.Anno}</span>`;
     let costo = document.createElement("span");
-    costo.innerHTML= `<span>Costo : ${prod.Costo} €</span>`;
+    costo.innerHTML= `<span>${prod.Costo} €</span>`;
     div.append(prodotto);
     prodotto.append(titolo);
     prodotto.append(artista);
