959aa5a19aa3363dd1c5ac0d3bc02b8b5659f59c
fradale2000 <fradale2000@gmail.com>
2022-06-30 10:40:08 +0200

    commit sviluppo

--- carrello.js
+++ carrello.js
@@ -4,25 +4,29 @@ function printCarll(){
     for (let i = 0; i < localStorage.length; i++){
         localStorage.getItem("song_" +i,JSON.stringify(prod));
         var prod = JSON.parse(localStorage.getItem("song_"+i));
+        let div_immagine = document.createElement("div");
         let immagine = document.createElement("img");
-            immagine.setAttribute("src",prod.src);
-            let titolo = document.createElement("span");
-            titolo.innerHTML= prod.Titolo;
-            let artista = document.createElement("span");
-            artista.innerHTML= prod.Artista;
-            let anno = document.createElement("span");
-            anno.innerHTML= prod.Anno;
-            let costo = document.createElement("span");
-            costo.innerHTML= prod.Costo+ " €";
+        immagine.classList.add("immagine");
+        div_immagine.classList.add("div_immagine");
+        div_immagine.appendChild(immagine);
+        immagine.setAttribute("src",prod.src);
+        let titolo = document.createElement("span");
+        titolo.innerHTML= prod.Titolo;
+        let artista = document.createElement("span");
+        artista.innerHTML= prod.Artista;
+        let anno = document.createElement("span");
+        anno.innerHTML= prod.Anno;
+        let costo = document.createElement("span");
+        costo.innerHTML= prod.Costo+ " €";
             
-            //appendo tutto nel div "record_carrello"
+        //appendo tutto nel div "record_carrello"
         let prodotto = document.createElement("div");
         prodotto.setAttribute("class","pro")
-            div.appendChild(prodotto);
-            prodotto.appendChild(immagine);
-            prodotto.appendChild(titolo);
-            prodotto.appendChild(artista);
-            prodotto.appendChild(anno);
-            prodotto.appendChild(costo);   
+        prodotto.appendChild(div_immagine);
+        prodotto.appendChild(titolo);
+        prodotto.appendChild(artista);
+        prodotto.appendChild(anno);
+        prodotto.appendChild(costo);
+        div.appendChild(prodotto);   
     }
 }
